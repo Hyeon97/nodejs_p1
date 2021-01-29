ce747a98ef780718b4f70b0de912d77b223535a8
배크준 <Hyeon97>
2021-01-29 19:02:26 +0900

    2번째

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ const port = 3000
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const { User } = require('./models/User')
-
+const config = require('./config/key')
 //
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-mongoose.connect("mongodb+srv://Admin:Admin@cluster0.vfnxd.mongodb.net/test?retryWrites=true&w=majority", {
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log("db connected..."))
     .catch(err => console.log(err))
